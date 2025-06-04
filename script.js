ecd53cb5fee8200b5b233819146e98e2dc8cbafa
Nathiel-bit <nathielsantos98@gmail.com>
2025-06-04 16:10:52 -0300

    Projeto

---
+++ script.js
@@ -0,0 +1,60 @@
+function gerarMapa() {
+    var largura = document.getElementById('width').value;
+    var altura = document.getElementById('height').value;
+
+    if (largura <= 0 || altura <= 0) {
+        alert("Largura e altura devem ser maiores que zero.");
+        return;
+    } else{
+        largura = parseInt(largura);
+        altura = parseInt(altura);
+        if (isNaN(largura) || isNaN(altura)) {
+            alert("Por favor, insira apenas números, meu caro amigo(a).");
+            return;
+        }
+    }
+
+    var canvas = document.getElementById('canvas');
+    canvas.width = largura * 100;
+    canvas.height = altura * 100;
+
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    for (var x = 0; x < largura; x++) {
+        for (var y = 0; y < altura; y++) {
+            ctx.strokeRect(x * 100, y * 100, 100, 100);
+        }
+    }
+}
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+
+largura = 5;
+altura = 5;
+canvas.width = largura * 100;
+canvas.height = altura * 100;
+mapa = [];
+
+function inicializadorArray(){
+for ( var x = 0; x < largura; x++){
+    mapa [x] = [];
+    for(var y = 0; y < altura; j++)
+        mapa[x].push = ( "vazio");
+    }
+}
+
+function printMapa(){
+for(var x = 0; x < largura; x++){
+    for(var y = 0; y < altura; y++){
+        if(mapa [x][y] == "vazio"){
+            ctx.strokeRect(x * 100, y * 100, 100, 100);
+        }
+    }
+ }
+}
+
+//inicializadorArray();
+//printMapa();
